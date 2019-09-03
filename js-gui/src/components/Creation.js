0d9112e8cd310b7d90b376afb4324fdb48de2494
Juan José Nieto <juanjo.3ns@gmail.com>
2019-09-03 15:43:36 +0100

    Who knows

--- js-gui/src/components/Creation.js
+++ js-gui/src/components/Creation.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import  { Breakpoint, BreakpointProvider } from 'react-socks';
+import { connect } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Grid from './Grid';
 import Parameters from './Parameters';
+import {
+  handleClick,
+  handleReset,
+} from '../actions';
 
 
 class Creation extends Component {
 
+    handleClick(e){
+      this.props.handleClick(e.target.id);
+    }
+    handleReset(e){
+      this.props.handleReset();
+    }
 
+    constructor(props) {
+      super(props);
+      this.state = {width: props.width};
+    }
+
+    componentWillMount(){
+      this.setState({width: window.innerWidth});
+    }
 
   render(){
-
+    console.log(this.state.width);
     return (
       <div style={{ display:'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'stretch', padding: '12px' }}>
         <BreakpointProvider>
@@ -17,26 +39,44 @@ class Creation extends Component {
             <div style={{ display:'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'stretch', padding: '5px' }}>
               <Parameters/>
               <Grid
-              preview={400}
+              preview={this.state.width}
               height={10}
               width={10}
               walls={[]}
               initstate={[]}
               finalstate={[]}
               />
+              <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                <Button onClick={this.handleReset.bind(this)} variant="danger">Reset</Button>
+                <ButtonGroup aria-label="Basic example">
+                  <Button id="walls" active={this.props.cell==="walls" ? true : false} onClick={this.handleClick.bind(this)} variant="secondary">Walls</Button>
+                  <Button id="initstate" active={this.props.cell==="initstate" ? true : false} onClick={this.handleClick.bind(this)} variant="secondary">Init state</Button>
+                  <Button id="finalstate" active={this.props.cell==="finalstate" ? true : false} onClick={this.handleClick.bind(this)} variant="secondary">Final state</Button>
+                </ButtonGroup>
+              </div>
             </div>
           </Breakpoint>
           <Breakpoint large up>
             <div style={{ display:'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'stretch', padding: '5px' }}>
               <Parameters/>
-              <Grid
-              preview={700}
-              height={10}
-              width={10}
-              walls={[]}
-              initstate={[]}
-              finalstate={[]}
-              />
+              <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-around" }}>
+                <Grid
+                preview={700}
+                height={10}
+                width={10}
+                walls={[]}
+                initstate={[]}
+                finalstate={[]}
+                />
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                  <Button onClick={this.handleReset.bind(this)} variant="danger">Reset</Button>
+                  <ButtonGroup aria-label="Basic example">
+                    <Button id="walls" active={this.props.cell==="walls" ? true : false} onClick={this.handleClick.bind(this)} variant="secondary">Walls</Button>
+                    <Button id="initstate" active={this.props.cell==="initstate" ? true : false} onClick={this.handleClick.bind(this)} variant="secondary">Init state</Button>
+                    <Button id="finalstate" active={this.props.cell==="finalstate" ? true : false} onClick={this.handleClick.bind(this)} variant="secondary">Final state</Button>
+                  </ButtonGroup>
+                </div>
+              </div>
             </div>
           </Breakpoint>
         </BreakpointProvider>
@@ -45,6 +85,12 @@ class Creation extends Component {
   }
 }
 
+const mapStateToProps = ({ generalbuttons, formValues }) => {
+  const { cell, walls, initstate, finalstate, walls_values } = generalbuttons;
+  return { cell, formValues , walls, initstate, finalstate, walls_values };
+}
 
-
-export default Creation;
+export default connect(mapStateToProps, {
+  handleClick,
+  handleReset,
+} )(Creation);
